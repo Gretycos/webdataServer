10da7b58f3015cf8d964939697a9312598c837b6
hyc <rogwel@vip.qq.com>
2020-04-13 00:42:34 +0800

    Initial commit

---
+++ src/test/java/com/webdata/WebdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebdataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
